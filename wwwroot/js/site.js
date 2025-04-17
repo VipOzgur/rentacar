c3154ad7a958e98be0806e8116dcffec32742f77
ridvancm <ridvan.cam46@gmail.com>
2025-04-17 22:19:25 +0300

    son hali carousel hariç buğradan çekildi

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -1,11 +1,11 @@
-const carousel = document.getElementById('carousel');
-const nextBtn = document.getElementById('nextBtn');
-const prevBtn = document.getElementById('prevBtn');
+//const carousel = document.getElementById('carousel');
+//const nextBtn = document.getElementById('nextBtn');
+//const prevBtn = document.getElementById('prevBtn');
 
-nextBtn.addEventListener('click', () => {
-  carousel.scrollBy({ left: 320, behavior: 'smooth' });
-});
+//nextBtn.addEventListener('click', () => {
+//  carousel.scrollBy({ left: 320, behavior: 'smooth' });
+//});
 
-prevBtn.addEventListener('click', () => {
-  carousel.scrollBy({ left: -320, behavior: 'smooth' });
-});
+//prevBtn.addEventListener('click', () => {
+//  carousel.scrollBy({ left: -320, behavior: 'smooth' });
+//});
